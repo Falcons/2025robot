038f810cc3a059b505275cf8e931ff006312dd69
madness <austinfaulknerjanes2008@gmail.com>
2025-02-27 18:38:39 -0500

    revert to analog + lasercan

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -39,7 +39,7 @@ public class RobotContainer {
   private final CommandXboxController driver = new CommandXboxController(0);
   private final CommandXboxController operator = new CommandXboxController(1);
 
-  private final double globalSpeedMod = 0.1;
+  private final double globalSpeedMod = 0.01;
   SendableChooser<Command> path_chooser = new SendableChooser<Command>();
   public RobotContainer() {
     CanBridge.runTCP();
