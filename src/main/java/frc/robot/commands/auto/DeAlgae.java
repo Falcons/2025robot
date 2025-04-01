58054bf9425cc8ce4f2e7a8eb70ba2fcb2923d06
cookie <38135845+complicatedstuff@users.noreply.github.com>
2025-04-01 16:57:22 -0400

    algae sequencual

--- src/main/java/frc/robot/commands/auto/DeAlgae.java
+++ src/main/java/frc/robot/commands/auto/DeAlgae.java
@@ -5,7 +5,11 @@
 package frc.robot.commands.auto;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants.ElevatorConstants;
+import frc.robot.commands.algae.pivotPidToggle;
+import frc.robot.commands.elevator.ElevatorTrapezoidalMove;
 import frc.robot.commands.moveToTarget.UnfilterdFollowTagG;
+import frc.robot.subsystems.algae.Intake;
 import frc.robot.subsystems.algae.Pivot;
 import frc.robot.subsystems.driveTrain.SwerveSubsystem;
 import frc.robot.subsystems.elevator.Elevator;
@@ -17,10 +21,12 @@ import frc.robot.subsystems.shooter.Coral;
 public class DeAlgae extends SequentialCommandGroup {
     double[] offset = {1,0,0};
     /** Creates a new relLimeL1. */
-  public DeAlgae(SwerveSubsystem swerve, Elevator elevator, Coral coral, Pivot pivot, double tagID) {
+  public DeAlgae(SwerveSubsystem swerve, Elevator elevator, Coral coral, Pivot pivot, Intake intake) {
     // Add your commands in the addCommands() call, e.g.
     addCommands(
-      new UnfilterdFollowTagG(swerve, offset)
+      new UnfilterdFollowTagG(swerve, offset),
+      new ElevatorTrapezoidalMove(elevator, ElevatorConstants.maxSpeed, ElevatorConstants.maxAcceleration, ElevatorConstants.algaeL3).asProxy(),
+      new pivotPidToggle(pivot)
      );
   }
 }
