4b1b6f219cedffed9823fe7b46b760182c687fcc
madness <austinfaulknerjanes2008@gmail.com>
2025-04-04 08:29:01 -0400

    auto changes

--- src/main/java/frc/robot/commands/auto/UnfilterdRelLimeL3.java
+++ src/main/java/frc/robot/commands/auto/UnfilterdRelLimeL3.java
@@ -31,8 +31,8 @@ public class UnfilterdRelLimeL3 extends SequentialCommandGroup {
       new PivotPid(pivot, AlgaeConstants.pivotKick),
       new ElevatorAndAlgae(elevator, intake, ElevatorConstants.coralL3).asProxy(),
       new relAutoDrive(swerve, new ChassisSpeeds(-0.25, 0, 0), 0.15),
-      new relAutoDrive(swerve, new ChassisSpeeds(0, lOrR, 0), 0.32),
-      new relAutoDrive(swerve, new ChassisSpeeds(0.5, 0, 0), 0.5),
+      new relAutoDrive(swerve, new ChassisSpeeds(0, lOrR, 0), 0.34),
+      new relAutoDrive(swerve, new ChassisSpeeds(0.5, 0, 0), 0.7),
       new RawShootForTime(coral, -0.23, -0.23, 2).asProxy()
      );
   }
